217eaef6d48fd829b94c62cf0633d99d4bac4618
NNB <43980777+NNBnh@users.noreply.github.com>
2021-04-15 10:11:22 +0700

    Update bai42.c

--- hk1/bai42.c
+++ hk1/bai42.c
@@ -6,44 +6,35 @@ int main() {
 
 	printf("Nhap so phan tu cua mang: "); scanf("%d", &n);
 	float a[n][n];
-	float hold;
 
-	for(i=0;i<n;i++) {
-		for(j=0;j<n;j++) {
-			printf("Nhap gia tri cua phan tu [%d][%d]: ", i, j); scanf("%f", &hold);
-
-			a[i][j]=hold;
+	for(int i=0;i<n;i++) {
+		for(int j=0;j<n;j++) {
+			printf("Nhap gia tri cua phan tu [%d][%d]: ", i, j); scanf("%f", &a[i][j]);
 		}
 	}
 
 	printf("\nGia tri cua cac phan tu trong mang la: ");
-	for(i=0;i<n;i++) {
+	for(int i=0;i<n;i++) {
 		printf("\n");
 
-		for(j=0;j<n;j++) {
-			hold=a[i][j];
-
-			printf("%f ", hold);
+		for(int j=0;j<n;j++) {
+			printf("%f ", a[i][j]);
 		}
 	}
 
 	printf("\nCac thanh phan cua cot mot la:");
-	for(i=0;i<n;i++) {
-		hold=a[i][0];
-
-		printf(" %f", hold);
+	for(int i=0;i<n;i++) {
+		printf(" %f", a[i][0]);
 	}
 
 	float tong;
-	for(j=0;j<n;j++) {
+	for(int j=0;j<n;j++) {
 		tong=0;
 
-		for(i=0;i<n;i++) {
+		for(int i=0;i<n;i++) {
 			tong+=a[i][j];
 		}
 
 		printf("\nTong cua cot [%d] la %f", j , tong);
 	}
-
-	printf("\n\n<3");
 }

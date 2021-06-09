82e67053e7eb490a4891904a76ad8d9d4c2b53d6
NNB <43980777+NNBnh@users.noreply.github.com>
2021-06-09 09:42:59 +0700

    Update tien.c

--- hk2/tien.c
+++ hk2/tien.c
@@ -3,32 +3,57 @@
 #include <malloc.h>
 #include <stdlib.h>
 
-void nhap(float * a, int n) {
-    float * p;
+// Cau 1
+void nhap(float *a, int n) {
+    float *p;
     for (p = a; p < a + n; p++) {
-        scanf("%f", p);
+        printf("Hay nhap phan tu %d: ", *p); scanf("%f", p);
     }
 }
 
-void hienthi(float * a, int n) {
-    float * p;
+// Cau 2
+void hienthi(float *a, int n) {
+    float *p;
     for (p = a; p < a + n; p++) {
-        printf("%5.1f", * p);
+        printf("%5.1f\n", *p);
     }
 }
 
+// Cau 3
+void tinhtong(float * a, int n) {
+    float *p, sum;
+    for (p = a; p < a + n; p++) {
+        sum+=*p;
+    }
+
+	printf("%f", sum);
+}
+
+// Cau 4
+void timvitri(float *a, int n) {
+    float *p, min = -99999, *max;
+    for (p = a; p < a + n; p++) {
+        if ( (*p > min) && (*p < 0) ) {
+			max = p;
+		}
+    }
+
+	printf("%f", max - a);
+}
+
+// Cau 6
 void ghitep(float * a, int n) {
-    FILE * f;
+    FILE *f;
     f = fopen("D:/FILE/mangthuc.txt", "wt");
     if (f == NULL) {
-        printf("tao file bi loi");
+        printf("\n! Tao file bi loi !");
         exit(1);
     }
     
-    float * p;
+    float *p;
     for (p = a; p < a + n; p++) {
-        if (( * p) < 10) {
-            fprintf(f, "%5.1f", * p);
+        if ((*p) > 0) {
+            fprintf(f, "%5.1f", *p);
         }
     }
     
@@ -36,15 +61,26 @@ void ghitep(float * a, int n) {
 }
 
 int main() {
-    float * a;
+    float *a;
     int n;
-    printf("nhap so luong phan tu:"); scanf("%d", & n);
+    printf("Hay nhap so luong phan tu: "); scanf("%d", & n);
     a = (float * ) malloc(n * sizeof(float));
     
+    // Cau 1
     nhap(a, n);
     
+    // Cau 2
     hienthi(a, n);
     
+    // Cau 3
+    printf("\nTong cac phan tu la ");
+    tinhtong(a, n);
+	
+    // Cau 4
+    printf("\nVi tri phan tu am lon nhat la ");
+	timvitri(a, n);
+
+    // Cau 6
     ghitep(a, n);
     
     free(a);
